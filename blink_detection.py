3c83071f9e72b5bcd13ffa92bfeb70cc3a3767e0
Abhishek <abhimore74@gmail.com>
2022-04-17 19:52:56 -0500

    eyebrow

--- blink_detection.py
+++ blink_detection.py
@@ -33,10 +33,17 @@ def mouth_aspect_ratio(mouth):
 	# return the mouth aspect ratio
 	return mar
 
+def eyebrow_aspect_ratio(eyebrow, eye):
+    A = dist.euclidean(eyebrow[2], eye[1]);
+    C = dist.euclidean(eyebrow[0], eyebrow[3]);
+
+    ebar = (A) / C
+    return ebar
 
 
 BLINK_THRESHOLD = 0.19  # the threshold of the ear below which we assume that the eye is closed
 MOUTH_AR_THRESH = 0.79
+EYEBROW_AR_THRESH = 1.5
 CONSEC_FRAMES_NUMBER = 2  # minimal number of consecutive frames with a low enough ear value for a blink to be detected
 
 # get arguments from a command line
@@ -54,6 +61,8 @@ predictor = dlib.shape_predictor(args["shape_predictor"])
 (left_s, left_e) = face_utils.FACIAL_LANDMARKS_IDXS["left_eye"]
 (right_s, right_e) = face_utils.FACIAL_LANDMARKS_IDXS["right_eye"]
 (mStart, mEnd) = (49, 68)
+(left_e_start, left_e_end) = (18,22)
+(right_e_start, right_e_end) = (23,27)
 
 # start the video stream or video reading from the file
 video_path = args["video"]
@@ -72,6 +81,7 @@ total = 0
 alert = False
 start_time = 0
 frame = vs.read()
+leftebar = 0
 
 # loop over the frames of video stream:
 # grab the frame, resize it, convert it to grayscale
@@ -81,6 +91,7 @@ while (not fileStream) or (frame is not None):
     gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     rects = detector(gray_frame, 0)
     ear = 0
+    ebar = 0
     # loop over the face detections:
     # determine the facial landmarks,
     # convert the facial landmark (x, y)-coordinates to a numpy array,
@@ -95,24 +106,29 @@ while (not fileStream) or (frame is not None):
         rightEAR = eye_aspect_ratio(rightEye)
         ear = (leftEAR + rightEAR) / 2.0
         mouth = shape[mStart:mEnd]
+        leftEyebrow = shape[left_e_start:left_e_end]
         mouthMAR = mouth_aspect_ratio(mouth)
+        lefteyebrowEBAR = eyebrow_aspect_ratio(leftEyebrow, leftEye) 
         mar = mouthMAR
+        leftebar = lefteyebrowEBAR
         # visualize each of the eyes
         leftEyeHull = cv2.convexHull(leftEye)
         rightEyeHull = cv2.convexHull(rightEye)
 		# compute the convex hull for the mouth, then
 		# visualize the mouth
         mouthHull = cv2.convexHull(mouth)
+        lefteyebrowHull = cv2.convexHull(leftEyebrow)
         cv2.drawContours(frame, [leftEyeHull], -1, (255, 0, 0), 1)
         cv2.drawContours(frame, [rightEyeHull], -1, (255, 0, 0), 1)
         cv2.drawContours(frame, [mouthHull], -1, (255, 0, 0), 1)
+        cv2.drawContours(frame, [lefteyebrowHull], -1, (255, 0, 0), 1)
 
         # if the eye aspect ratio is below the threshold, increment counter
         # if the eyes are closed longer than for 2 secs, raise an alert
         if ear < BLINK_THRESHOLD:
             counter += 1
             if start_time == 0:
-                print("BLINKED AT " + str(time.time()))
+                #print("BLINKED AT " + str(time.time()))
                 start_time = time.time()
             else:
                 end_time = time.time()
@@ -126,13 +142,19 @@ while (not fileStream) or (frame is not None):
         if mar > MOUTH_AR_THRESH:
             cv2.putText(frame, "Mouth is Open!", (30,60),
 			cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0,0,255),2)
+        if leftebar > EYEBROW_AR_THRESH:
+            cv2.putText(frame, "Eyebrow Raised!", (30,90),
+			cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0,0,255),2)
+
+
 
     # draw the total number of blinks and EAR value
     cv2.putText(frame, "Blinks: {}".format(total), (10, 30),
                 cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 0), 2)
     #cv2.putText(frame, "EAR: {:.2f}".format(ear), (500, 30),
     #            cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 0), 2)
-
+    cv2.putText(frame, "Blinks: {}".format(leftebar), (40, 30),
+                cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 0), 2)
     #cv2.putText(frame, "MAR: {:.2f}".format(mar), (30, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
     if alert:
         cv2.putText(frame, "ALERT!", (150, 30),
